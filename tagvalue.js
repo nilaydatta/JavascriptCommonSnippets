29f41f0d8ee1fe82fad81cc46537951763d78e40
Nilay Datta <nilay.datta@live.com>
2021-11-13 22:17:25 +0530

    Create tagvalue.js
    Iterate over array and create new array with another layout. Includes grouping and arranging data.

---
+++ tagvalue.js
@@ -0,0 +1,452 @@
+const arr = [
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-10-29T19:31:00.000Z",
+    tagvalue: 12.69
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-10-30T19:31:00.000Z",
+    tagvalue: 6.09
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-10-31T19:31:00.000Z",
+    tagvalue: 10.96
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-01T19:31:00.000Z",
+    tagvalue: 9.2
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-02T19:31:00.000Z",
+    tagvalue: 1.89
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-03T19:31:00.000Z",
+    tagvalue: 5.6
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-04T19:31:00.000Z",
+    tagvalue: 5.25
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-05T19:31:00.000Z",
+    tagvalue: 1.84
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-06T19:31:00.000Z",
+    tagvalue: 11.48
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-07T19:31:00.000Z",
+    tagvalue: 9.99
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-08T19:31:00.000Z",
+    tagvalue: 1.53
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-09T19:31:00.000Z",
+    tagvalue: 13.08
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-10T19:31:00.000Z",
+    tagvalue: 7.19
+  },
+  {
+    unid: "HJEK3922.FT",
+    sr_datetime: "2021-11-11T19:31:00.000Z",
+    tagvalue: 15.04
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-10-29T19:31:00.000Z",
+    tagvalue: 19.22
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-10-30T19:31:00.000Z",
+    tagvalue: 241.57
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-10-31T19:31:00.000Z",
+    tagvalue: 263.12
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-01T19:31:00.000Z",
+    tagvalue: 391.46
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-02T19:31:00.000Z",
+    tagvalue: 70.96
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-03T19:31:00.000Z",
+    tagvalue: 351.4
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-04T19:31:00.000Z",
+    tagvalue: 586.24
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-05T19:31:00.000Z",
+    tagvalue: 126.22
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-06T19:31:00.000Z",
+    tagvalue: 601.61
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-07T19:31:00.000Z",
+    tagvalue: 770.93
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-08T19:31:00.000Z",
+    tagvalue: 408.23
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-09T19:31:00.000Z",
+    tagvalue: 255.62
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-10T19:31:00.000Z",
+    tagvalue: 659.28
+  },
+  {
+    unid: "P314H.TP.PV",
+    sr_datetime: "2021-11-11T19:31:00.000Z",
+    tagvalue: 490.43
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-10-29T19:31:00.000Z",
+    tagvalue: 63.645
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-10-30T19:31:00.000Z",
+    tagvalue: 31.406
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-10-31T19:31:00.000Z",
+    tagvalue: 27.772
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-01T19:31:00.000Z",
+    tagvalue: 91.397
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-02T19:31:00.000Z",
+    tagvalue: 52.285
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-03T19:31:00.000Z",
+    tagvalue: 26.935
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-04T19:31:00.000Z",
+    tagvalue: 25.294
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-05T19:31:00.000Z",
+    tagvalue: 64.236
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-06T19:31:00.000Z",
+    tagvalue: 1.677
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-07T19:31:00.000Z",
+    tagvalue: 3.368
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-08T19:31:00.000Z",
+    tagvalue: 44.701
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-09T19:31:00.000Z",
+    tagvalue: 39.026
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-10T19:31:00.000Z",
+    tagvalue: 17.095
+  },
+  {
+    unid: "P594H.FR.PV",
+    sr_datetime: "2021-11-11T19:31:00.000Z",
+    tagvalue: 81.047
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-10-29T19:31:00.000Z",
+    tagvalue: 75
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-10-30T19:31:00.000Z",
+    tagvalue: 22
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-10-31T19:31:00.000Z",
+    tagvalue: 43
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-01T19:31:00.000Z",
+    tagvalue: 99
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-02T19:31:00.000Z",
+    tagvalue: 82
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-03T19:31:00.000Z",
+    tagvalue: 84
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-04T19:31:00.000Z",
+    tagvalue: 68
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-05T19:31:00.000Z",
+    tagvalue: 96
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-06T19:31:00.000Z",
+    tagvalue: 39
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-07T19:31:00.000Z",
+    tagvalue: 24
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-08T19:31:00.000Z",
+    tagvalue: 28
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-09T19:31:00.000Z",
+    tagvalue: 67
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-10T19:31:00.000Z",
+    tagvalue: 15
+  },
+  {
+    unid: "P416H.STAT",
+    sr_datetime: "2021-11-11T19:31:00.000Z",
+    tagvalue: 68
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-10-29T19:31:00.000Z",
+    tagvalue: 12
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-10-30T19:31:00.000Z",
+    tagvalue: 36
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-10-31T19:31:00.000Z",
+    tagvalue: 5
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-01T19:31:00.000Z",
+    tagvalue: 19
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-02T19:31:00.000Z",
+    tagvalue: 28
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-03T19:31:00.000Z",
+    tagvalue: 13
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-04T19:31:00.000Z",
+    tagvalue: 45
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-05T19:31:00.000Z",
+    tagvalue: 13
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-06T19:31:00.000Z",
+    tagvalue: 23
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-07T19:31:00.000Z",
+    tagvalue: 9
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-08T19:31:00.000Z",
+    tagvalue: 21
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-09T19:31:00.000Z",
+    tagvalue: 40
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-10T19:31:00.000Z",
+    tagvalue: 38
+  },
+  {
+    unid: "VFE93.SS",
+    sr_datetime: "2021-11-11T19:31:00.000Z",
+    tagvalue: 45
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-10-29T19:31:00.000Z",
+    tagvalue: 66.8
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-10-30T19:31:00.000Z",
+    tagvalue: 252.92
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-10-31T19:31:00.000Z",
+    tagvalue: 185.3
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-01T19:31:00.000Z",
+    tagvalue: 96.35
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-02T19:31:00.000Z",
+    tagvalue: 141.32
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-03T19:31:00.000Z",
+    tagvalue: 99.08
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-04T19:31:00.000Z",
+    tagvalue: 225.4
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-05T19:31:00.000Z",
+    tagvalue: 32.75
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-06T19:31:00.000Z",
+    tagvalue: 55.54
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-07T19:31:00.000Z",
+    tagvalue: 47.33
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-08T19:31:00.000Z",
+    tagvalue: 285.6
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-09T19:31:00.000Z",
+    tagvalue: 238.1
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-10T19:31:00.000Z",
+    tagvalue: 113.28
+  },
+  {
+    unid: "P456H.FR.PV",
+    sr_datetime: "2021-11-11T19:31:00.000Z",
+    tagvalue: 232.57
+  }
+];
+
+
+const result = [];
+const obj = {};
+
+// Looping through input array and grouping required data in an object.
+// This is without lodash
+for(const item of arr) {
+  if(obj.hasOwnProperty(item.unid)) {
+    obj[item.unid].push(item.tagvalue);
+  } else {
+    obj[item.unid] = [];
+    obj[item.unid].push(item.tagvalue);
+  }
+}
+
+// Constructing final array
+for(const item in obj) {
+  const base = {
+    label: item,
+    data: obj[item]
+  }
+  result.push(base);
+}
+
+// Checking final result
+result.forEach(function(el) {
+  console.log(el);
+});
+
